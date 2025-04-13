b9865856f5628a65ec2875c0dd906f360e4fb2b2
Tonosaki <tonosaki914@icloud.com>
2025-04-13 17:14:33 +0900

    api/create

--- src/app/api/item/create/route.ts
+++ src/app/api/item/create/route.ts
@@ -1,5 +1,17 @@
 import { NextResponse } from "next/server";
+import { supabase } from "@/app/utils/database";
 
-export async function GET() {
-  return NextResponse.json({ message: "アイテム作成" });
+export async function POST(request: Request) {
+  const reqBody = await request.json();
+  console.log(reqBody);
+
+  try {
+    await supabase.from("items").insert(reqBody);
+    return NextResponse.json({ message: "アイテム作成成功" });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "アイテム作成失敗：：" + error },
+      { status: 500 }
+    );
+  }
 }
