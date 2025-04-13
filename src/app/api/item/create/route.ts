3bb210c825da55d1b4f437eb623f73f73311a007
Tonosaki <tonosaki914@icloud.com>
2025-04-13 17:38:58 +0900

    item/readall

--- src/app/api/item/create/route.ts
+++ src/app/api/item/create/route.ts
@@ -3,14 +3,21 @@ import { supabase } from "@/app/utils/database";
 
 export async function POST(request: Request) {
   const reqBody = await request.json();
-  console.log(reqBody);
 
   try {
-    await supabase.from("items").insert(reqBody);
-    return NextResponse.json({ message: "アイテム作成成功" });
+    const { data, error } = await supabase
+      .from("items")
+      .insert(reqBody)
+      .select();
+
+    if (error) {
+      throw error;
+    }
+
+    return NextResponse.json({ message: "アイテム作成成功", data });
   } catch (error) {
     return NextResponse.json(
-      { message: "アイテム作成失敗：：" + error },
+      { message: "アイテム作成失敗：" + error },
       { status: 500 }
     );
   }
--- src/app/api/item/readall/route.ts
+++ src/app/api/item/readall/route.ts
@@ -1,7 +1,19 @@
 import { NextResponse } from "next/server";
+import { supabase } from "@/app/utils/database";
 
-export async function POST(request: Request) {
-  console.log(await request.json());
-  //   const { name, price } = await request.json();
-  return NextResponse.json({ message: "アイテム取得" });
+export async function GET() {
+  try {
+    const { data, error } = await supabase.from("items").select();
+
+    if (error) {
+      throw error;
+    }
+
+    return NextResponse.json({ message: "読み取り成功", data });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "読み取り失敗：" + error },
+      { status: 500 }
+    );
+  }
 }
