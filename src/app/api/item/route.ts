b53b8c7b7886a93b6147b66f4a1c1dac82909c5c
Tonosaki <tonosaki914@icloud.com>
2025-04-13 20:16:48 +0900

    ディレクトリ構造の修正

--- src/app/api/item/route.ts
+++ src/app/api/item/route.ts
@@ -20,3 +20,28 @@ export async function GET() {
     );
   }
 }
+
+export async function POST(request: Request) {
+  const reqBody = await request.json();
+
+  try {
+    const { data, error } = await supabase
+      .from("items")
+      .insert(reqBody)
+      .select();
+
+    if (error) {
+      throw error;
+    }
+
+    return NextResponse.json({
+      message: "アイテム作成成功",
+      data,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "アイテム作成失敗：" + error },
+      { status: 500 }
+    );
+  }
+}
