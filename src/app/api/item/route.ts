be6e05097f026acd593c5105144d48b0b62062c6
Tonosaki <tonosaki914@icloud.com>
2025-04-15 10:19:15 +0900

    キャッシュ処理追加

--- src/app/api/item/route.ts
+++ src/app/api/item/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/app/utils/database";
 
+export const revalidate = 0;
+
 export async function GET() {
   try {
     const { data, error } = await supabase.from("items").select();
